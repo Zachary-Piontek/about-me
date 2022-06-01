082672468ec28c87222973320b9f9cbf3cbc3e21
Zachary Piontek <fortunefavorsthebrave@protonmail.com>
2022-06-01 15:14:19 -0700

    TOS enable button

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 // grab DOM elements
+const tosCheckbox = document.getElementById('tos-checkbox');
+const displayAnimal = document.getElementById('display-animal');
+
 
 // set event listeners
+
+tosCheckbox.addEventListener('change', () => {
+    displayAnimal.disabled = !tosCheckbox.checked;
+    // learned how case sensitive JS can be. Is there any way to check
+    // if this happens in your code, as the code will have no errors show?
+});
+
     // get info from user input
     // use user input to update state
     // update DOM to reflect the new state
