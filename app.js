985eb2124ae732fdb3301536e0b5203f974e07a0
Zachary Piontek <fortunefavorsthebrave@protonmail.com>
2022-06-01 15:41:59 -0700

    button shows animal section

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // grab DOM elements
 const tosCheckbox = document.getElementById('tos-checkbox');
 const displayAnimal = document.getElementById('display-animal');
-
+const animalSection = document.getElementById('animal-section');
 
 // set event listeners
 
@@ -9,6 +9,10 @@ tosCheckbox.addEventListener('change', () => {
     displayAnimal.disabled = !tosCheckbox.checked;
     // learned how case sensitive JS can be. Is there any way to check
     // if this happens in your code, as the code will have no errors show?
+});
+
+displayAnimal.addEventListener('click', () => {
+    animalSection.classList.remove('hidden');
 });
 
     // get info from user input
